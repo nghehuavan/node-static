82d26eeb4844d47200423688556a0d9d67be11c3
nghe.huavan <nghe.huavan@gmail.com>
2024-03-10 08:55:40 +0700

    [UPD-API]

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-app.use(express.static(path.join(__dirname, '../dist')));
+app.use(express.static(path.join(__dirname, '../static')));
 app.set('port', 3000);
 const server = app.listen(app.get('port'), () => {
   console.log(`Express running → ${server.address().port} [http://localhost:${server.address().port}]`);
