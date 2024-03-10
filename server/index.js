8b0aa5e7bb01f7c26db7e2949d63cd7eae3cc081
nghe.huavan <nghe.huavan@gmail.com>
2024-03-10 08:48:54 +0700

    .

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 app.use(express.static(path.join(__dirname, '../dist')));
-app.set('port', 8081);
+app.set('port', 3000);
 const server = app.listen(app.get('port'), () => {
   console.log(`Express running → ${server.address().port} [http://localhost:${server.address().port}]`);
 });
