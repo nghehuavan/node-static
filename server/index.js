21a1c698e58ef29f0b53ddcdce9e91fdc3dd5227
nghe.huavan <nghe.huavan@gmail.com>
2024-04-09 12:25:16 +0700

    [UPD-API]

--- server/index.js
+++ server/index.js
@@ -1,37 +1,33 @@
 const path = require('path');
 const express = require('express');
 const multer = require('multer');
-const fs = require('fs');
 const app = express();
-const upload = multer({ dest: 'uploads/' });
+const upload = multer();
 
 app.post('/upload', upload.single('file'), (req, res) => {
-  fs.readFile(req.file.path, (err, data) => {
-    if (err) throw err;
-    const base64Image = Buffer.from(data).toString('base64');
+  const fileBuffer = req.file.buffer;
+  const base64String = fileBuffer.toString('base64');
+  let json = {
+    id: '155baaa9-7646-4012-8031-3a53a40434e3',
+    name: 'test.jpg',
+    time: 1666686753,
+    items: [
+      {
+        amount: 120,
+        quantity: 10,
+      },
+      {
+        amount: 100,
+        quantity: 2,
+      },
+    ],
+    loss: 2,
+    total: 1400,
+    image: base64String,
+    csv: 'MSwxNTViYWFhOS03NjQ2LTQwMTItODAzMS0zYTUzYTQwNDM0ZTMsMSwy',
+  };
 
-    let json = {
-      id: '155baaa9-7646-4012-8031-3a53a40434e3',
-      name: 'test.jpg',
-      time: 1666686753,
-      items: [
-        {
-          amount: 120,
-          quantity: 10,
-        },
-        {
-          amount: 100,
-          quantity: 2,
-        },
-      ],
-      loss: 2,
-      total: 1400,
-      image: base64Image,
-      csv: 'MSwxNTViYWFhOS03NjQ2LTQwMTItODAzMS0zYTUzYTQwNDM0ZTMsMSwy',
-    };
-
-    res.json(json);
-  });
+  res.json(json);
 });
 
 app.use(express.static(path.join(__dirname, '../static')));
